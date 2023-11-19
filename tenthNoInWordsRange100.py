ed031a9752c64f665a7ccdb0fda97295f007480d
Mukesh Yadav <116198746+mukeshylearn@users.noreply.github.com>
2023-11-19 22:26:29 +0530

    Update tenthNoInWordsRange100.py

--- tenthNoInWordsRange100.py
+++ tenthNoInWordsRange100.py
@@ -1,3 +1,4 @@
+# script that will print the numbers from 0 to 100 and convert every tenth to a wordy version
 from num2words import num2words
 print("PRINTING NUMBERS FROM 0 TO 100")
 for i in range(0,101):
